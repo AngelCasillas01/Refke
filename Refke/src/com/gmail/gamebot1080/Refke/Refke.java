a668442638db2aee0ddea0de178b6c94b67883bd
javipepe <javipepeoctc@gmail.com>
2015-01-24 14:19:21 +0100

    /(un)mute and Listeners (PlayerCommandPreprocessEvent, PlayerChatEvent) completed. Closes issue #2.
    Usage: Mute (and unmute!) Players and prevent them from executing any command meanwhile.

--- Refke/src/com/gmail/gamebot1080/Refke/Refke.java
+++ Refke/src/com/gmail/gamebot1080/Refke/Refke.java
@@ -31,19 +31,27 @@ public class Refke extends JavaPlugin {
       Player p = e.getPlayer();
 
       if(mplayers.contains(p.getName)){
-         e.setCancelled(true)
+         e.setCancelled(true);
          
          p.sendMessage(ChatColor.RED + "Message was not sent because you are muted!");
       }
 
 
     }
+    
+    @EventHandler
+    public void onCommandProccess(PlayerCommandPreprocessEvent e){
+      Player p = e.getPlayer();
+
+      if(mplayers.contains(p.getName)){
+         e.setCancelled(true);
+         
+         p.sendMessage(ChatColor.RED + "Message was not sent because you are muted!");
+      }
+    }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String CommandLabel, String[] args) {
-       
-               
     	 Player player = (Player)sender;
-         
          if(CommandLabel.equalsIgnoreCase("ref")){
                  if(args.length == 0){
                          player.sendMessage(ChatColor.GREEN + "Referees: "); player.sendMessage(ChatColor.AQUA + "{PLAYERS}");
@@ -52,18 +60,66 @@ public class Refke extends JavaPlugin {
                 
                 
          }
+       return false;
+     }
 
+ @Override
+ public boolean onCommand(CommandSender sender, Command command, String CommandLabel, String[] args) {
          if(CommandLabel.equalsIgnoreCase("mute")){
            if(sender.isOp()){
             if(args.length == 0){
                sender.sendMessage(ChatColor.RED + "/mute <player>");
+            }else if(!(args.length > 1)){
+               Player target = Bukkit.getPlayer(args[0]);
+               
+               if(target == null){
+                 sender.sendMessage(ChatColor.RED + "Player not online!");
+                 return false;
+               }
+               
+               if(mplayers.contains(target.getName())){
+                 sender.sendMessage(ChatColor.DARK_RED + target.getDisplayName() + ChatColor.YELLOW + " is already muted! Use /unmute " + target.getName() + " to unmute him/her!");
+                 return false;
+               }
+               
+               mplayers.add(target.getName);
+               sender.sendMessage(ChatColor.YELLOW + "You muted " + target.getDisplayName());
+               target.sendMessage(ChatColor.YELLOW + "You were muted by " + sender.getDisplayName());
+               return true;
             }
            }
-         }
-
-
-
- return false;
-
-}
+         }return false;
+  
+ } 
+ 
+ 
+  @Override
+ public boolean onCommand(CommandSender sender, Command command, String CommandLabel, String[] args) {
+         if(CommandLabel.equalsIgnoreCase("unmute")){
+           if(sender.isOp()){
+            if(args.length == 0){
+               sender.sendMessage(ChatColor.RED + "/unmute <player>");
+            }else if(!(args.length > 1)){
+               Player target = Bukkit.getPlayer(args[0]);
+               
+               if(target == null){
+                 sender.sendMessage(ChatColor.RED + "Player not online!");
+                 return false;
+               }
+               
+               if(mplayers.contains(target.getName()){
+                 sender.sendMessage(target.getDisplayName() + ChatColor.YELLOW + " is not muted. Use /mute " + target.getName() + " to mute him/her!");
+                 return false;
+               }
+               
+               mplayers.add(target.getName);
+               sender.sendMessage(ChatColor.YELLOW + "You unmuted " + target.getDisplayName());
+               target.sendMessage(ChatColor.YELLOW + "You were unmuted by " + sender.getDisplayName());
+               return true;
+            }
+           }
+         }return false;
+  
+ } 
+ 
 }

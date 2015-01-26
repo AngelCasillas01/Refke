5c6a480764523a07d3e5b327cb3fb4dcd826ef09
vYuri <vYuri@users.noreply.github.com>
2015-01-26 01:34:41 -0300

    Added the command: /heal and /feed

--- Refke/src/com/gmail/gamebot1080/Refke/Refke.java
+++ Refke/src/com/gmail/gamebot1080/Refke/Refke.java
@@ -76,6 +76,53 @@ public class Refke extends JavaPlugin {
        return false;
      }
 
+@Override 
+         public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+               
+                if (!(sender instanceof Player)) {
+                        sender.sendMessage(ChatColor.RED + "The console cannot use heal!");
+                        return true;
+                }
+               
+                Player player = (Player) sender;
+               
+                if (cmd.getName().equalsIgnoreCase("heal")) {
+                        if (args.length == 0) {
+                                player.setHealth(20);
+                                player.sendMessage(ChatColor.GREEN + "You have been healed!");
+                                return true;
+                        }
+                        Player target = Bukkit.getServer().getPlayer(args[0]);
+                        if (target == null) {
+                                player.sendMessage(ChatColor.RED + "Could not find player!");
+                                return true;
+                        }
+                        target.setHealth(20);
+                        target.sendMessage(ChatColor.GREEN + "You have been healed!");
+                        player.sendMessage(ChatColor.GREEN + target.getName() + " has been healed!");
+                }
+               
+                if (cmd.getName().equalsIgnoreCase("feed")) {
+                        if (args.length == 0) {
+                                player.setFoodLevel(20);
+                                player.sendMessage(ChatColor.GREEN + "You have been fed!");
+                                return true;
+                        }
+                        Player target = Bukkit.getServer().getPlayer(args[0]);
+                        if (target == null) {
+                                player.sendMessage(ChatColor.RED + "Could not find player!");
+                                return true;
+                        }
+                        target.setFoodLevel(20);
+                        target.sendMessage(ChatColor.GREEN + "You have been fed!");
+                        player.sendMessage(ChatColor.GREEN + target.getName() + " has been fed!");
+                 }
+                
+                
+         }
+       return false;
+     }
+
  @Override
  public boolean onCommand(CommandSender sender, Command command, String CommandLabel, String[] args) {
          if(CommandLabel.equalsIgnoreCase("mute")){
